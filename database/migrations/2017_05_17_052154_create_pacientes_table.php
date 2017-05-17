a9c0966c6ac47957499729534f7a3c282ec95b42
Leonardo Cherubini <Leonardo Cherubini>
2017-05-17 02:24:15 -0300

    Adicao da migration de Paciente

---
+++ database/migrations/2017_05_17_052154_create_pacientes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePacientesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('Paciente', function (Blueprint $table) {
+            $table->increments('IdPaciente');
+            $table->string('Nome')->default('');
+            $table->string('CPF')->default('');
+            $table->string('NomePai')->default('');
+            $table->string('NomeMae')->default('');
+            $table->string('RG')->default('');
+            $table->string('Endereco')->default('');
+            $table->string('Telefone')->default('');
+            $table->string('Email')->default('');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('Paciente');
+    }
+}
